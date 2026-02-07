23618cc7671bcbea4ef4976a615581f61269943c
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2026-02-07 08:49:29 +0100

    chore: initialize Symfony 8.0 project from API Platform

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->notPath([
+        'config/reference.php',
+    ])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+;
